f838f2edbdfdc9ec93bb0a45b70e11cf01f0f813
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2026-02-09 13:06:39 +0545

    wip

---
+++ resources/views/livewire/products-section.blade.php
@@ -0,0 +1,48 @@
+<div class="pt-60 px-60">
+    <div>
+        <div class="flex justify-center items-center flex-col text-center gap-5">
+            <h4 class="w-fit px-4 py-2 text-sm bg-primary/5 text-primary rounded-full">Products By Grid Layers</h4>
+            <div class="px-60">
+                <h4 class="text-5xl font-bold leading-snug">One platform, <span
+                        class="font-black text-primary">multiple</span> business products</h4>
+            </div>
+        </div>
+        <div class="mt-20 flex justify-center">
+            <div class="w-100 rounded-xl bg-linear-to-tl from-primary/3 shadow-xs p-10 space-y-5 border border-black/3">
+                <div class="h-14 w-14 bg-primary rounded-2xl flex justify-center items-center text-white">
+                    <x-heroicon-m-academic-cap class="h-8 w-8" />
+                </div>
+                <div class="space-y-2">
+                    <h2 class="text-2xl font-bold">Training Institutes</h2>
+                    <p class="text-black/60">Complete management suite for modern educational centers and private
+                        tutors.</p>
+                </div>
+                <div class="space-y-5">
+                    <div class="flex gap-2 items-center text-sm">
+                        <x-heroicon-m-check-circle class="h-6 w-6 text-primary" />
+                        <span>Website & SEO</span>
+                    </div>
+                    <div class="flex gap-2 items-center text-sm">
+                        <x-heroicon-m-check-circle class="h-6 w-6 text-primary" />
+                        <span>Student Portal</span>
+                    </div>
+                    <div class="flex gap-2 items-center text-sm">
+                        <x-heroicon-m-check-circle class="h-6 w-6 text-primary" />
+                        <span>Automated Payments</span>
+                    </div>
+                    <div class="flex gap-2 items-center text-sm">
+                        <x-heroicon-m-check-circle class="h-6 w-6 text-primary" />
+                        <span>Attendance Tracking</span>
+                    </div>
+                </div>
+                <div class="space-y-5 mt-8">
+                    <button class="btn btn-primary h-14 w-full">Explore Product</button>
+                    <div class="flex justify-center">
+                        <a href="#"class="text-primary text-sm font-medium inline-flex gap-1 items-center">View
+                            Demo <x-heroicon-m-arrow-right class="h-4 w-4" /> </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
