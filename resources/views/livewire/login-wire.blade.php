9bd19da788a626802a37787009901fd62454e017
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2026-02-11 11:08:57 +0545

    wip

---
+++ resources/views/livewire/login-wire.blade.php
@@ -0,0 +1,37 @@
+<div>
+    <div class="grid grid-cols-2 h-screen">
+        <div class="flex justify-center items-center">
+            <div class="flex flex-col items-center justify-center gap-2">
+                <div class="h-16 w-16">
+                    <img src="{{ asset('images/gridlayer.png') }}" alt="" class="h-full w-full object-contain">
+                </div>
+                <h2 class="font-bold text-2xl">Welcome Back</h2>
+                <div>
+                    <button class="btn bg-black/4 w-80 h-12 rounded-2xl">
+                        <div class="h-5 w-fit">
+                            <img class="h-full w-full object-contain"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/250px-Google_%22G%22_logo.svg.png"
+                                alt="">
+                        </div>
+                        <span>Continue with Google</span>
+                    </button>
+                </div>
+                <div class="divider text-sm">Or</div>
+                <div class="grid gap-3">
+                    <input type="text" placeholder="Enter Your Email"
+                        class="input w-80 h-12 font-semibold text-base" />
+                    <input type="password" placeholder="• • • • • • • • • •"
+                        class="input w-80 h-12 font-semibold text-base" />
+                    <button class="btn btn-primary w-80 h-12">Login</button>
+                </div>
+                <div class="flex justify-center text-sm text-black/60 font-medium mt-5">
+                    <span>Don't Have Account ? <a href="" class="text-primary font-semibold">Sign Up</a></span>
+                </div>
+            </div>
+        </div>
+        <div>
+            <img src="https://ubrand.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcollaboration-en.2bbfa642.png&w=3840&q=75"
+                alt="" class="h-full w-full object-contain">
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Home;
+use App\Livewire\LoginWire;
 use App\Livewire\Onboarding;
 use App\Livewire\ProductViewWire;
 
 Route::livewire('/', Home::class)->name('platform.home');
 Route::livewire('/product/{slug}', ProductViewWire::class)->name('platform.product');
 Route::get('/onboarding', Onboarding::class)->name('platform.onboarding')->fallback(fn() => redirect()->route('platform.home'));
+Route::livewire('login', LoginWire::class)->name('platform.login');
