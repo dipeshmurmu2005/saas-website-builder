8ec8e70e6e32be0fd532e243bb8419720fa786fc
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2026-02-13 14:18:06 +0545

    wip

--- resources/views/components/⚡footer-wire.blade.php
+++ resources/views/components/⚡footer-wire.blade.php
@@ -7,8 +7,141 @@ new class extends Component {
 };
 ?>
 
-<div class="px-60 pt-32">
-    <div>
-
+<div class="px-60 pt-20 mt-10 border-t border-black/3">
+    <div class="grid grid-cols-5 text-sm">
+        <div>
+            <h3 class="font-semibold">Intergations</h3>
+            <div class="grid space-y-2 mt-3 text-black/70 font-['inter']">
+                <a href="#" class="w-fit flex gap-1 items-center">
+                    <div class="h-4 w-4">
+                        <img src="https://brandeps.com/logo-download/G/Google-Analytics-logo-01.png" alt=""
+                            class="h-full w-full object-contain">
+                    </div>
+                    Google Analytics
+                </a>
+                <a href="#" class="w-fit flex gap-1 items-center">
+                    <div class="h-4 w-4">
+                        <img src="https://cdn.worldvectorlogo.com/logos/google-adsense-logo-2019-.svg" alt=""
+                            class="h-full w-full object-contain">
+                    </div>
+                    Google Adsense
+                </a>
+                <a href="#" class="w-fit flex gap-1 items-center">
+                    <div class="h-4 w-4">
+                        <img src="https://marketplace-assets.digitalocean.com/logos/chatwoot.png" alt=""
+                            class="h-full w-full object-contain">
+                    </div>
+                    Chatwoot
+                </a>
+                <a href="#" class="w-fit flex gap-1 items-center">
+                    <div class="h-4 w-4">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/WhatsApp_Logo_green.svg/1280px-WhatsApp_Logo_green.svg.png"
+                            alt="" class="h-full w-full object-contain">
+                    </div>
+                    WhatsApp
+                </a>
+                <a href="#" class="w-fit flex gap-1 items-center">
+                    <div class="h-4 w-4">
+                        <img src="https://mallastock.com/media/img/fonepay.png" alt=""
+                            class="h-full w-full object-contain">
+                    </div>
+                    Fonepay
+                </a>
+                <a href="#" class="w-fit flex gap-1 items-center">
+                    <div class="h-4 w-4">
+                        <img src="https://cdn.esewa.com.np/ui/images/logos/esewa-icon-large.png" alt=""
+                            class="h-full w-full object-contain">
+                    </div>
+                    Esewa
+                </a>
+                <a href="#" class="w-fit flex gap-1 items-center">
+                    <div class="h-4 w-4">
+                        <img src="https://images.icon-icons.com/2407/PNG/512/mailchimp_icon_146054.png" alt=""
+                            class="h-full w-full object-contain">
+                    </div>
+                    Mailchimp
+                </a>
+                <a href="#" class="w-fit flex gap-1 items-center">
+                    <div class="h-4 w-4">
+                        <img src="https://www.drupal.org/files/styles/grid-4-2x/public/_brand_assets_images_logos_zapier-logomark.png?itok=N_Vj8dAq"
+                            alt="" class="h-full w-full object-contain">
+                    </div>
+                    Zapier
+                </a>
+                <a href="#" class="w-fit flex gap-1 items-center">
+                    <div class="h-4 w-4">
+                        <img src="https://hackerprep.io/companies/stripe.png" alt=""
+                            class="h-full w-full object-contain">
+                    </div>
+                    Stripe
+                </a>
+                <a href="#" class="w-fit flex gap-1 items-center">
+                    <div class="h-4 w-4">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Slack_icon_2019.svg/500px-Slack_icon_2019.svg.png"
+                            alt="" class="h-full w-full object-contain">
+                    </div>
+                    Slack
+                </a>
+            </div>
+        </div>
+        <div>
+            <h3 class="font-semibold">Solutions</h3>
+            <div class="grid space-y-2 mt-3 text-black/70 font-['inter']">
+                <a href="#" class="w-fit">Grid Layers for Institutes</a>
+                <a href="#" class="w-fit">Grid Layers for Cafe</a>
+                <a href="#" class="w-fit">Grid Layers for Restaurants</a>
+                <a href="#" class="w-fit">Grid Layers for Co-working Spaces</a>
+                <a href="#" class="w-fit">Grid Layers for Shops</a>
+            </div>
+        </div>
+        <div>
+            <h3 class="font-semibold">Links</h3>
+            <div class="grid space-y-2 mt-3 text-black/70 font-['inter']">
+                <a href="#" class="w-fit">Join Partner Program</a>
+                <a href="#" class="w-fit">Open Startup</a>
+                <a href="#" class="w-fit">Documentation</a>
+                <a href="#" class="w-fit">Tutorials</a>
+                <a href="#" class="w-fit">Pricing & Plans</a>
+                <a href="#" class="w-fit">API Reference</a>
+                <a href="#" class="w-fit">Blog</a>
+                <a href="#" class="w-fit">Community Forum</a>
+                <a href="#" class="w-fit">FAQs</a>
+                <a href="#" class="w-fit">Careers</a>
+            </div>
+        </div>
+        <div>
+            <h3 class="font-semibold">Documentation</h3>
+            <div class="grid space-y-2 mt-3 text-black/70 font-['inter']">
+                <a href="#" class="w-fit">Getting Started</a>
+                <a href="#" class="w-fit">Installation Guide</a>
+                <a href="#" class="w-fit">API Reference</a>
+                <a href="#" class="w-fit">Integration Guide</a>
+                <a href="#" class="w-fit">Grid Layers Setup</a>
+                <a href="#" class="w-fit">Payment Gateway Docs</a>
+                <a href="#" class="w-fit">Partner Program Docs</a>
+                <a href="#" class="w-fit">FAQs</a>
+                <a href="#" class="w-fit">Changelog</a>
+                <a href="#" class="w-fit">Tutorials</a>
+            </div>
+        </div>
+        <div class="space-y-2">
+            <div class="h-12 w-12">
+                <img src="{{ asset('images/gridlayer.png') }}" alt="" class="h-full w-full">
+            </div>
+            <div class="space-y-2">
+                <h2 class="font-semibold">Grid Layers</h2>
+                <p class="text-sm text-black/70">From billing to revenue recognition, streamline your entire
+                    contract-to-cash process
+                    and eliminate
+                    manual work. </p>
+            </div>
+        </div>
+    </div>
+    <div class="pt-32 flex justify-center">
+        <span class="text-sm text-black/60">© 2025 ongridlayers.com. All rights reserved.</span>
+    </div>
+    <div
+        class="text-[14rem] font-black flex justify-center italic bg-linear-to-l select-none from-primary/40 to-[#BF40BF]/10 bg-clip-text">
+        <span class="text-transparent">Grid Layers</span>
     </div>
 </div>
